8173ffde6c45365f74a87e0ae557b559e2c017ed
C4R4DD1CT <152124665+C4R4DD1CT@users.noreply.github.com>
2024-03-01 00:39:10 -0800

    Actually added dragon to dragon scene

--- TopTierStudios-TSA-Game/Assets/Scripts/MusicPlayerPersistance.cs
+++ TopTierStudios-TSA-Game/Assets/Scripts/MusicPlayerPersistance.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 /// <summary>
@@ -11,6 +12,14 @@ using UnityEngine;
 public class MusicPlayerPersistance : MonoBehaviour
 {
     public AudioSource music;
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Dragon Scene"))
+        {
+            Destroy(transform.gameObject);
+        }
+    }
 
     // Don't destroy the object on new load and subscribe to game events
     void Start()
